e42589b04b4c6c1f00df8911189a8b72d5a3035d
matheuscods <matheus.santos.maggio@gmail.com>
2024-09-15 17:58:47 -0300

    feat: add aula03

---
+++ aula03.js
@@ -0,0 +1,21 @@
+let n1= 10;
+let n2= 10;
+let n3= 10;
+let n4= 10;
+let media = (n1+n2+n3+n4)/4;
+
+if (media>=6) {
+console.log(`O aluno passou, com a média: ${media}`)
+} else {
+    console.log(`O aluno não passou. média: ${media}`)
+}
+
+let idadedoc= 18;
+let idaderg= 17;
+if (idadedoc>=18||idaderg>=18) {
+    console.log(`O aluno é maior de idade. Idade: ${idadedoc} anos`)
+} else {
+    console.log(`O aluno não é maior de idade. idade: ${idadedoc}`)
+}
+
+console.log('gIT TESTE')
